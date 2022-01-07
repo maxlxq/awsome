40f9efe7e9a8a75f50ad040d9ca49320bc76138e
maxlxq <542168513@qq.com>
2022-01-08 01:18:51 +0800

    feat: mini change

--- packages/awsome-utils/src/index.ts
+++ packages/awsome-utils/src/index.ts
@@ -2,8 +2,8 @@
  * @Author: shizhi
  * @Date: 2022-01-06 23:49:23
  * @LastEditors: shizhi
- * @LastEditTime: 2022-01-07 23:03:52
- * @description: 
+ * @LastEditTime: 2022-01-08 01:18:39
+ * @description: 工具库
  */
 /// <reference path="../../../typings.d.ts" />
 
