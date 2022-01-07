55c3e3b537d437b6c74d4e4b621f9059414907c6
maxlxq <542168513@qq.com>
2022-01-08 01:28:48 +0800

    chore(release): publish v0.0.5

--- packages/awsome-utils/src/index.ts
+++ packages/awsome-utils/src/index.ts
@@ -2,8 +2,8 @@
  * @Author: shizhi
  * @Date: 2022-01-06 23:49:23
  * @LastEditors: shizhi
- * @LastEditTime: 2022-01-08 01:18:39
- * @description: 工具库
+ * @LastEditTime: 2022-01-08 01:28:35
+ * @description: 工具库-utils
  */
 /// <reference path="../../../typings.d.ts" />
 
