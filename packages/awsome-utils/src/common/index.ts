6064edabcb92aaabd640658c073562cd6a627c02
maxlxq <542168513@qq.com>
2022-01-08 23:50:42 +0800

    feat: change some

--- packages/awsome-utils/src/common/index.ts
+++ packages/awsome-utils/src/common/index.ts
@@ -2,8 +2,8 @@
  * @Author: shizhi
  * @Date: 2022-01-07 00:00:47
  * @LastEditors: shizhi
- * @LastEditTime: 2022-01-07 22:51:14
- * @description: 
+ * @LastEditTime: 2022-01-08 23:49:59
+ * @description: 测试 trim
  */
 
 export * from './trim';
