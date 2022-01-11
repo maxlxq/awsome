7cfd1b5ffbbc535ae799a6296f8789a9f32e0bab
maxlxq <542168513@qq.com>
2022-01-11 23:41:35 +0800

    fix: package.json

--- packages/awsome-utils/src/common/index.ts
+++ packages/awsome-utils/src/common/index.ts
@@ -2,16 +2,18 @@
  * @Author: shizhi
  * @Date: 2022-01-07 00:00:47
  * @LastEditors: shizhi
- * @LastEditTime: 2022-01-09 23:28:14
+ * @LastEditTime: 2022-01-11 23:41:19
  * @description: 公用方法
  */
 
 import depsAreaSame from './depsAreaSame';
 import isFunction from './isFunction';
+import isBrowser from './isBrowser';
 
 export {
   depsAreaSame,
   isFunction,
+  isBrowser,
 };
 
 export * from './trim';
